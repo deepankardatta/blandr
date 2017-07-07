1c04df0e8bc9c73ed412b97799009ca4ff4cd399
Deepankar Datta <deepankardatta@users.noreply.github.com>
2017-07-07 14:01:36 +0100

    more updates for CRAN submission

--- unused/submit-cran.r
+++ unused/submit-cran.r
@@ -1,10 +1,14 @@
 # http://r-pkgs.had.co.nz/release.html
 
+# RUN-ONCE
 # devtools::use_readme_rmd()
+# devtools::use_build_ignore("NEWS.md")
+
+# RUN every build
 devtools::document()
 devtools::check()
-# devtools::build_win()
-devtools::revdep_check()
-devtools::use_build_ignore("NEWS.md")
 
+# RUN final build
+# devtools::build_win()
+# devtools::revdep_check()
 devtools::release()
