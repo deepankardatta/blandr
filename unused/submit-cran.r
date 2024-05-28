7186d3ce094068cbe698b30e89ace3308e69f396
Deepankar Datta <1972312+deepankardatta@users.noreply.github.com>
2024-05-28 21:37:45 +0100

    Uploading fixes

--- unused/submit-cran.r
+++ unused/submit-cran.r
@@ -1,10 +1,10 @@
 # http://r-pkgs.had.co.nz/release.html
 
 # RUN-ONCE
-# devtools::use_readme_rmd()
-# devtools::use_build_ignore("NEWS.md")
-# devtools::use_cran_comments()
-# devtools::use_testthat()
+#devtools::use_readme_rmd()
+#devtools::use_build_ignore("NEWS.md")
+#devtools::use_cran_comments()
+#devtools::use_testthat()
 
 # RUN every build
 devtools::document()
